e3401358ce20555ae1832a0d0167a7fefdb81b34
Marco Caimi <mcaimi@redhat.com>
2022-07-15 17:34:21 +0200

    Remove BROKER_XML env var and only patch config files loaded from filesystem.
    As these files are already mounted from a configmap, the BROKER_XML
    variable is now useless and redundant and can lead to a non-booting
    container if the broker.xml content is larger than the maximum size of
    the env var.

--- artemis-broker/scripts-override/configure_custom_config.sh
+++ artemis-broker/scripts-override/configure_custom_config.sh
@@ -2,9 +2,8 @@
 set -e
 
 INSTANCE_DIR=$1
-DISABLER_TAG="<!-- Remove this tag to enable custom configuration -->"
 
-declare -a CONFIG_FILES=("BOOTSTRAP_XML" "BROKER_XML" "LOGGING_PROPERTIES")
+declare -a CONFIG_FILES=("bootstrap.xml" "broker.xml" "logging.properties")
 
 function swapVars() {
   # Requires bash v4+
@@ -29,27 +28,8 @@ function swapVars() {
 
 for config_file in ${CONFIG_FILES[@]};
 do
-  file_text="${!config_file}"
-  file_text=$(echo "$file_text" | sed  "/^$/d") # Remove empty lines
-
-  # Format env var into filename 
-  fname=$(echo "$config_file" | tr '[:upper:]' '[:lower:]' | sed -e 's/_/./g')
-
-  #If file_text has disabler tag or is an empty/whitspace string 
-  if echo "$file_text" | grep -q "$DISABLER_TAG" || [[ -z "${file_text// }" ]]; then  
-
-    echo "Custom Configuration file '$config_file' is disabled"
-
-  else
-
-    echo "Custom Configuration file '$config_file' is enabled"
-
-    # Overwrite default configuration file
-    echo "$file_text" > $INSTANCE_DIR/etc/$fname
-
-  fi
-
-    # Swap env vars into configuration file
-    swapVars $INSTANCE_DIR/etc/$fname
+  # Swap env vars into configuration file
+  echo "Patching Custom Configuration file '$config_file'"
+  swapVars $INSTANCE_DIR/etc/$config_file
 done
 
